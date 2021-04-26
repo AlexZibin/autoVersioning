5732d39fa1b0aff3b93a3861f60e3e57774cc68c
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2021-04-26 19:22:00 +0300

    Update build_defs.h

--- build_defs.h
+++ build_defs.h
@@ -119,7 +119,7 @@
 //builds a version that looks like 1.140216.2041
 const char TimestampedVersion[] =
 {
-    'V', 'E', 'R', '.', ' ', 7, 
+    'V', 'E', 'R', '.', ' ', '_', 
     VERSION_MAJOR_INIT,
     '.',
     BUILD_YEAR_CH0,
